57f4b471559fb99f375d5097446be7dbe1b05253
Chavi Goyal <goyalchhavi599@gmail.com>
2026-07-17 14:44:06 +0530

    main commit

---
+++ ex1.js
@@ -0,0 +1,13 @@
+// let a =10;
+// const b=25;
+// console.log( "a=", a);
+// console.log("b=",b);
+// b=a++;
+// console.log("b=",b);
+
+function sum(a,b){
+    return a+b;
+
+}
+
+console.log("sum=", sum(10,20));
